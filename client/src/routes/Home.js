343842bae22aa847b80c433aa2ad26e687787806
Gage Eide <gageeide@gmail.com>
2020-07-17 10:52:22 -0500

    Added Men's/Women's Tab buttons under banner

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import '../stylesheets/Home.scss';
 
 //components
 import AText from "../components/AText";
@@ -7,15 +8,24 @@ import Banner from '../components/Banner';
 
 const Home = () => {
     return(
-        <>
-        <Header 
-            title="Welcome to My Stripes!" 
-            headerClass="Title-Header"
-            divClass="Container-Header"
-            hClass="Text-Header"
-        /> {/*header with custom title*/}
-        <Banner />
-        </>
+        <main>
+            <Header 
+                title="Welcome to My Stripes!" 
+                headerClass="Title-Header"
+                divClass="Container-Header"
+                hClass="Text-Header"
+            /> {/*header with custom title*/}
+            <Banner />
+            <section className="featured-collections">
+                <header>
+                    <h3 className="sub-heading">NEW ARRIVALS</h3>
+                    <div className="tab-button-container">
+                        <button className="tab-item is-active">MENS</button>
+                        <button className="tab-item">WOMENS</button>
+                    </div>
+                </header>
+            </section>
+        </main>
     );
 };
 
