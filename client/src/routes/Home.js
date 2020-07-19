67256d19e69380b4f3ad3f021b7b5371cb2c7ddd
Shannon Scirghio <shanscirg7@gmail.com>
2020-07-19 13:01:03 -0700

    working on styling product images

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/Home.scss';
 
 
@@ -6,6 +6,9 @@ import '../stylesheets/Home.scss';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Image from '../components/Image';
+
+// importing product list from db
+// import products from '';
 
 class Home extends React.Component {
     constructor(props) {
@@ -16,17 +19,23 @@ class Home extends React.Component {
     }
 
     render() {
-        return(
+        return (
             <div className="content-wrap">
                 <Banner />
                 <section className="featured-collections">
                     <header>
                         <h3 className="sub-heading">NEW ARRIVALS</h3>
                         <div className="tab-button-container">
-                            <button className={this.state.activeSection === 'mens' ? "tab-item is-active" : "tab-item"} onClick={() => this.setState({activeSection: "mens"})}>MENS</button>
-                            <button className={this.state.activeSection === 'womens' ? "tab-item is-active" : "tab-item"} onClick={() => this.setState({activeSection: "womens"})}>WOMENS</button>
+                            <button className={this.state.activeSection === 'mens' ? "tab-item is-active" : "tab-item"} onClick={() => this.setState({ activeSection: "mens" })}>MENS</button>
+                            <button className={this.state.activeSection === 'womens' ? "tab-item is-active" : "tab-item"} onClick={() => this.setState({ activeSection: "womens" })}>WOMENS</button>
                         </div>
                     </header>
+                    {/* Render product list from db and map through to display images */}
+                    {/* {products.map(item => (
+                        <Image
+                            item={item}
+                        />
+                    ))} */}
                 </section>
             </div>
         );
