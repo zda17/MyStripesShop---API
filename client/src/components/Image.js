0da9066df19df71e576823be63dbb8890933dafb
Shannon Scirghio <shanscirg7@gmail.com>
2020-07-20 10:16:13 -0700

    showing dummy images on home page to display shirts and pants

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Image = ({ to, imgDivClass, imgClass, src, alt }) => {
+    // const { to, imgDivClass, imgClass, src, alt } = props.product;
     return (
-        <div class={imgDivClass}>
+        <div className={imgDivClass}>
             <Link to={to}>
-                <img class={imgClass} src={src} alt={alt} />
+                <img className={imgClass} src={src} alt={alt} />
             </Link>
         </div>
     );
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../stylesheets/Home.scss';
+import '../stylesheets/Image.scss';
 
 
 //components
@@ -9,6 +10,10 @@ import Image from '../components/Image';
 
 // importing product list from db
 // import products from '';
+
+// import images
+import tshirt from '../utils/images/tshirt.png';
+import pants from '../utils/images/pants.png';
 
 class Home extends React.Component {
     constructor(props) {
@@ -30,12 +35,108 @@ class Home extends React.Component {
                             <button className={this.state.activeSection === 'womens' ? "tab-item is-active" : "tab-item"} onClick={() => this.setState({ activeSection: "womens" })}>WOMENS</button>
                         </div>
                     </header>
-                    {/* Render product list from db and map through to display images */}
-                    {/* {products.map(item => (
+                    <section className='products'>
+
+                        {/* Render product list from db and .map through to display images */}
+                        {/* {products.map(product => (
                         <Image
-                            item={item}
+                            product={product}
                         />
                     ))} */}
+
+                        {/* Displaying hard-coded image components until we get a product list to .map through */}
+                        {this.state.activeSection === 'mens' ?
+                            <>
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={tshirt}
+                                    alt='T-shirt'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={tshirt}
+                                    alt='T-shirt'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={tshirt}
+                                    alt='T-shirt'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={tshirt}
+                                    alt='T-shirt'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={tshirt}
+                                    alt='T-shirt'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={tshirt}
+                                    alt='T-shirt'
+                                />
+                            </>
+                            :
+                            <>
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={pants}
+                                    alt='Pants'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={pants}
+                                    alt='Pants'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={pants}
+                                    alt='Pants'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={pants}
+                                    alt='Pants'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={pants}
+                                    alt='Pants'
+                                />
+                                <Image
+                                    to='/Product.js'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    src={pants}
+                                    alt='Pants'
+                                />
+                            </>
+                        }
+                    </section>
                 </section>
             </div>
         );
