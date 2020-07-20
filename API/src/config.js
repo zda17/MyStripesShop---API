d103699f485fd19149e7f2c088ed226a05059a4a
Gage Eide <gageeide@gmail.com>
2020-07-20 17:59:12 -0500

    added DATABASE_URL to config.js and added comments to app.js

--- API/src/config.js
+++ API/src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development'
+  NODE_ENV: process.env.NODE_ENV || 'development',
+
+  DATABASE_URL: process.env.DATABASE_URL
+    || 'postgresql://postgres@localhost/mystripes',
 };
