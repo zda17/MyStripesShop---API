4b2b9f49ccb6ebd5919863ca8657ac6af2931323
Gage Eide <gageeide@gmail.com>
2020-07-24 20:09:54 -0500

    Added database calls for Womens and Unisex

--- API/src/products/product-service.js
+++ API/src/products/product-service.js
@@ -3,10 +3,19 @@ const ProductService = {
 		return db('products')
 			.select('*');
 	},
+	// Gender based product queries
 	getAllMens(db) {
 		return db('products')
 			.where({gender: 'M'});
-	}
+	},
+	getAllWomens(db) {
+		return db('products')
+			.where({gender: 'F'});
+	},
+	getAllUnisex(db) {
+		return db('products')
+			.where({gender: 'U'});
+	},
 };
 
 module.exports = ProductService;
