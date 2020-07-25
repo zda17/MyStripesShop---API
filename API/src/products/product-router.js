3313a7871fa73cdaa3f66d18adea482d15ff2521
Gage Eide <gageeide@gmail.com>
2020-07-24 20:12:34 -0500

    Added routes for womens and unisex

--- API/src/products/product-router.js
+++ API/src/products/product-router.js
@@ -15,5 +15,20 @@ productRouter
 	.get(async (req, res, next) => {
 		const mensProducts = await ProductService.getAllMens(req.app.get('db'));
 		res.send({mensProducts});
-	})
+	});
+
+productRouter
+	.route('/womens')
+	.get(async (req, res, next) => {
+		const womensProducts = await ProductService.getAllWomens(req.app.get('db'));
+		res.send({womensProducts});
+	});
+
+productRouter
+	.route('/unisex')
+	.get(async (req, res, next) => {
+		const unisexProducts = await ProductService.getAllUnisex(req.app.get('db'));
+		res.send({unisexProducts});
+	});
+
 module.exports = productRouter
