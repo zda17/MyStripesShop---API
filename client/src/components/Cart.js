acc5286957be6a6741699e56b9b60062282b8484
Morgan Houston <morgan.houston@okstate.edu>
2020-07-25 14:07:30 -0500

    Got some data to display in cart
    Only size, color, and price. For some reason I can't get the item name to display.

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,14 +1,55 @@
 import React, { Component, useState, useContext } from 'react';
 import { render } from "react-dom";
 import SlidingPane from "react-sliding-pane";
-import "react-sliding-pane/dist/react-sliding-pane.css";
+
+//assets
 import cartImg from '../utils/images/cart.png';
 import { CartContext } from '../utils/CartContext';
+
+//routes
+
+
+//components
+import Image from "../components/Image";
+
+//stlye
+import "react-sliding-pane/dist/react-sliding-pane.css";
 import "../stylesheets/Cart.scss";
 
 //cart item component to insert into cart pane
-const CartItem = () => {
-    return(null);
+const CartItem = (props) => {
+
+    const { product } = props;
+
+    const [cart, setCart] = useContext(CartContext);
+
+    return(
+        <div className="cart-item-wrapper">
+                
+                {/*lists all items in cart*/}
+                {cart.map(product => (
+                    <div className="cart-item">
+                        <div className="cart-image">
+                            <Image
+                                    to='/Product'
+                                    imgDivClass='img-div-home'
+                                    imgClass='product-img-home'
+                                    product={product}
+                            />
+                        </div>
+                        <div className="cart-info">
+                            <h2>{product.name}</h2>
+                                <span><p>{product.size} ~ {product.color}</p></span>
+                            <span>${product.price}</span>
+                        </div>
+                        <div className="cart-options">
+                            <p>Remove buton goes here</p>
+                        </div>
+                    </div>
+                ))}
+
+        </div>
+    );
 }
 
 
@@ -17,6 +58,7 @@ const Cart = () => {
     //used to pass cart array
     const [cart, setCart] = useContext(CartContext);
 
+    //gets total price
     const totalPrice = cart.reduce((acc, curr) => acc + curr.price, 0);
 
     //states for cart pane
@@ -43,11 +85,8 @@ const Cart = () => {
                     }}
                 >
                     {/*test data*/}
-                    <div>
-                        <span>Items in cart: {cart.length}</span>
-                        <br />
-                        <span>Total price: {totalPrice}</span>
-                        
+                    <div className="cart-item-list">
+                        <CartItem />
                     </div>
                 </SlidingPane>
             </div>
--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -48,43 +48,43 @@ const ProductForm = (props) => {
                   <div className="Colors">
                     <ul>
                         <li className={darkBlue}>{/*<-- prop used to display the colors or not*/}
-                            <input type="radio" name="color" id="darkBlue" value="darkBlue" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="darkBlue" value="Dark Blue" ref={register({ required: true })}/>
                             <label className="darkBlue" htmlFor="darkBlue"><span className="darkBlue"></span><span className="darkBlue__selector"/></label>
                         </li>
                         <li className={blue}>
-                            <input type="radio" name="color" id="blue" value="blue" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="blue" value="BLUE" ref={register({ required: true })}/>
                             <label htmlFor="blue"><span className="blue"></span><span className="blue__selector"/></label>
                         </li>
                         <li className={green}>
-                            <input type="radio" name="color" id="green" value="green" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="green" value="GREEN" ref={register({ required: true })}/>
                             <label htmlFor="green"><span className="green"></span><span className="green__selector"/></label>
                         </li>
                         <li className={peach}>
-                            <input type="radio" name="color" id="peach" value="peach" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="peach" value="PEACH" ref={register({ required: true })}/>
                             <label htmlFor="peach"><span className="peach"></span><span className="peach__selector"/></label>
                         </li>
                         <li className={gold}>
-                            <input type="radio" name="color" id="gold" value="gold" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="gold" value="GOLD" ref={register({ required: true })}/>
                             <label htmlFor="gold"><span className="gold"></span><span className="gold__selector"/></label>
                         </li>
                         <li className={red}>
-                            <input type="radio" name="color" id="red" value="red" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="red" value="RED" ref={register({ required: true })}/>
                             <label htmlFor="red"><span className="red"></span><span className="red__selector"/></label>
                         </li>
                         <li className={yellow}>
-                            <input type="radio" name="color" id="yellow" value="yellow" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="yellow" value="YELLOW" ref={register({ required: true })}/>
                             <label htmlFor="yellow"><span className="yellow"></span><span className="yellow__selector"/></label>
                         </li>
                         <li className={orange}>
-                            <input type="radio" name="color" id="orange" value="orange" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="orange" value="ORANGE" ref={register({ required: true })}/>
                             <label htmlFor="orange"><span className="orange"></span><span className="orange__selector"/></label>
                         </li>
                         <li className={purple}>
-                            <input type="radio" name="color" id="purple" value="purple" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="purple" value="PURPLE" ref={register({ required: true })}/>
                             <label htmlFor="purple"><span className="purple"></span><span className="purple__selector"/></label>
                         </li>
                         <li className={pink}>
-                            <input type="radio" name="color" id="pink" value="pink" ref={register({ required: true })}/>
+                            <input type="radio" name="color" id="pink" value="PINK" ref={register({ required: true })}/>
                             <label htmlFor="pink"><span className="pink"></span><span className="pink__selector"/></label>
                         </li>
                     </ul>
@@ -102,31 +102,31 @@ const ProductForm = (props) => {
                  <div className="Sizes">
                     <ul>
                         <li className={xs}> {/*<--prop used for showing out of order (not made yet)*/}
-                            <input type="radio" name="size" id="x-small" value="x-small" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="x-small" value="X-SMALL" ref={register({ required: true })}/>
                             <label htmlFor="x-small"><span className="x-small">XS</span></label>
                         </li>
                         <li className={s}>
-                            <input type="radio" name="size" id="small" value="small" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="small" value="SMALL" ref={register({ required: true })}/>
                             <label htmlFor="small"><span className="small">S</span></label>
                         </li>
                         <li className={m}>
-                            <input type="radio" name="size" id="medium" value="medium" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="medium" value="MEDIUM" ref={register({ required: true })}/>
                             <label htmlFor="medium"><span className="medium">M</span></label>
                         </li>
                         <li className={l}>
-                            <input type="radio" name="size" id="large" value="large" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="large" value="LARGE" ref={register({ required: true })}/>
                             <label htmlFor="large"><span className="large">L</span></label>
                         </li>
                         <li className={xl}>
-                            <input type="radio" name="size" id="x-large" value="x-large" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="x-large" value="X-LARGE" ref={register({ required: true })}/>
                             <label htmlFor="x-large"><span className="x-large">XL</span></label>
                         </li>
                         <li className={xxl}>
-                            <input type="radio" name="size" id="xx-large" value="xx-large" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="xx-large" value="XX-LARGE" ref={register({ required: true })}/>
                             <label htmlFor="xx-large"><span className="xx-large">XXL</span></label>
                         </li>
                         <li className={xxxl}>
-                            <input type="radio" name="size" id="xxx-large" value="xxx-large" ref={register({ required: true })}/>
+                            <input type="radio" name="size" id="xxx-large" value="XXX-LARGE" ref={register({ required: true })}/>
                             <label htmlFor="xxx-large"><span className="xxx-large">XXXL</span></label>
                         </li>
                     </ul>
