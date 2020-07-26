014e82cd409cb169c6e45a150d7735b4bccafdc9
Gage Eide <gageeide@gmail.com>
2020-07-26 15:23:17 -0500

    Added more accurate dummyProduct

--- client/src/routes/Product.js
+++ client/src/routes/Product.js
@@ -16,7 +16,8 @@ const Product = () => {
             updated_at: '2020-07-20 11:13:06', 
             price_cents: 3599, 
             size: 'S',
-            color: 'green'
+            color: 'green',
+            quantity: 4
         },
         {
             sku: 'MSCM-Hoodie-1-S-R', 
@@ -29,7 +30,8 @@ const Product = () => {
             updated_at: '2020-07-20 11:13:06', 
             price_cents: 3599, 
             size: 'S',
-            color: 'red'
+            color: 'red',
+            quantity: 4
         },
         {
             sku: 'MSCM-Hoodie-1-S-B', 
@@ -42,7 +44,8 @@ const Product = () => {
             updated_at: '2020-07-20 11:13:06', 
             price_cents: 3599, 
             size: 'S',
-            color: 'blue'
+            color: 'blue',
+            quantity: 4
         },
         {
             sku: 'MSCM-Hoodie-1-M-G', 
@@ -55,7 +58,8 @@ const Product = () => {
             updated_at: '2020-07-20 11:13:06', 
             price_cents: 3599, 
             size: 'M',
-            color: 'green'
+            color: 'green',
+            quantity: 4
         },
         {
             sku: 'MSCM-Hoodie-1-M-R', 
@@ -68,10 +72,11 @@ const Product = () => {
             updated_at: '2020-07-20 11:13:06', 
             price_cents: 3599, 
             size: 'M',
-            color: 'red'
+            color: 'red',
+            quantity: 4
         },
         {
-            sku: 'MSCM-Hoodie-1-M-R', 
+            sku: 'MSCM-Hoodie-1-M-B', 
             name: 'My Stripes Hoodie', 
             category: 'tops', 
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
@@ -81,7 +86,50 @@ const Product = () => {
             updated_at: '2020-07-20 11:13:06', 
             price_cents: 3599, 
             size: 'M',
-            color: 'red'
+            color: 'blue',
+            quantity: 4
+        },
+        {
+            sku: 'MSCM-Hoodie-1-L-G', 
+            name: 'My Stripes Hoodie', 
+            category: 'tops', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
+            gender: 'U', 
+            photo_url: 'https://res.cloudinary.com/dswxhdeob/image/upload/v1581615690/MyStripesProducts/My%20Stripes%20Custom%20Merch/My_Stripes_Custom_Merch_Hoodie_gla8a9.png', 
+            created_at: '2020-07-20 11:13:06', 
+            updated_at: '2020-07-20 11:13:06', 
+            price_cents: 3599, 
+            size: 'L',
+            color: 'green',
+            quantity: 4
+        },
+        {
+            sku: 'MSCM-Hoodie-1-L-R', 
+            name: 'My Stripes Hoodie', 
+            category: 'tops', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
+            gender: 'U', 
+            photo_url: 'https://res.cloudinary.com/dswxhdeob/image/upload/v1581615690/MyStripesProducts/My%20Stripes%20Custom%20Merch/My_Stripes_Custom_Merch_Hoodie_gla8a9.png', 
+            created_at: '2020-07-20 11:13:06', 
+            updated_at: '2020-07-20 11:13:06', 
+            price_cents: 3599, 
+            size: 'L',
+            color: 'red',
+            quantity: 4
+        },
+        {
+            sku: 'MSCM-Hoodie-1-L-B', 
+            name: 'My Stripes Hoodie', 
+            category: 'tops', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
+            gender: 'U', 
+            photo_url: 'https://res.cloudinary.com/dswxhdeob/image/upload/v1581615690/MyStripesProducts/My%20Stripes%20Custom%20Merch/My_Stripes_Custom_Merch_Hoodie_gla8a9.png', 
+            created_at: '2020-07-20 11:13:06', 
+            updated_at: '2020-07-20 11:13:06', 
+            price_cents: 3599, 
+            size: 'L',
+            color: 'blue',
+            quantity: 4
         },
     ]
 
