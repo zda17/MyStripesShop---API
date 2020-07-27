2a866e6e608cba38457b00d22e376e46b92188d6
Shannon Scirghio <shanscirg7@gmail.com>
2020-07-27 09:45:15 -0700

    added checkoutform js and scss files

---
+++ client/src/components/CheckoutForm.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import '../stylesheets/CheckoutForm.scss';
+
+
+const CheckoutForm = () => {
+    return(
+        null
+    )
+}
+
+
+export default CheckoutForm;
