993605c977db6ed751bf05c4317e1d4a744b6b99
Gage Eide <gageeide@gmail.com>
2020-07-31 11:01:54 -0500

    Removed unnecessary console.log

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Image = (props) => {
-    console.log(props.product);
     const { photo_url, name } = props.product;
     const { to, imgDivClass, imgClass } = props;
     return (
