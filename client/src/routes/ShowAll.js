1550b71838b84249dac15705aad3c55aea1fd103
Morgan Houston <morgan.houston@okstate.edu>
2020-08-03 19:16:01 -0500

    Created a sample 'ShowAll' page
    Shows all products on load, then can filter to all tops, all bottoms and all misc. Basically copied and pasted gage's work from home page.

--- client/src/routes/ShowAll.js
+++ client/src/routes/ShowAll.js
@@ -1,7 +1,118 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect, Profiler } from 'react';
+import '../stylesheets/Home.scss';
+import '../stylesheets/Image.scss';
 
-const ShowAll = () => {
-    return(null);
+// Axios
+import axios from '../utils/axios';
+
+//components
+import Image from '../components/Image';
+
+const ShowAllProducts = () => {
+
+    const [activeSection, setActiveSection] = useState('all');
+    const [topsProduct, setTopsProduct] = useState([]);
+    const [bottomsProduct, setBottomsProduct] = useState([]);
+    const [miscProduct, setMiscProduct] = useState([]);
+    const [allProduct, setAllProduct] = useState([]);
+
+    useEffect(() => {
+        axios.get('/products/all')
+            .then(res => {
+                const product = res.data;
+                setAllProduct(product);
+                setTopsProduct(product.filter(item => item.category === 'tops'));
+                setBottomsProduct(product.filter(item => item.category === 'bottoms'));
+                setMiscProduct(product.filter(item => item.category === 'accessories'));
+            });
+    }, []);
+
+    return (
+        <div className="content-wrap" >
+            <section className="featured-collections">
+                <header>
+                    <h3 className="sub-heading">ALL CLOTHING ARTICLES</h3>
+                    <div className="tab-button-container">
+                        <button className={activeSection === 'all' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("all")}>ALL</button>
+                        <button className={activeSection === 'tops' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("tops")}>TOPS</button>
+                        <button className={activeSection === 'bottoms' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("bottoms")}>BOTTOMS</button>
+                        <button className={activeSection === 'accessories' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("accessories")}>MISC</button>
+                    </div>
+                </header>
+                <section className='products'>
+                    {activeSection === 'tops' ?
+                        <>
+                            {topsProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to={`/Product/${product.base_sku}`}
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                        : activeSection === 'bottoms' ?
+                        <>
+                            {bottomsProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to='/Product'
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                        : activeSection === 'accessories' ?
+                        <>
+                            {miscProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to='/Product'
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                        :
+                        <>
+                            {allProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to='/Product'
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                    }
+                </section>
+            </section>
+        </div>
+    );
 };
 
-export default ShowAll;
+export default ShowAllProducts;
