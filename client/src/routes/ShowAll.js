21267f3c119b0f36b4a26f6fb05ce183efcb227d
Gage Eide <gageeide@gmail.com>
2020-08-03 19:32:21 -0500

    Implemented Morgan's cool filter

--- client/src/routes/ShowAll.js
+++ client/src/routes/ShowAll.js
@@ -9,12 +9,20 @@ import Image from '../components/Image';
 
 function ShowAll() {
 
-    const [product, setProduct] = useState([]);
+    const [activeSection, setActiveSection] = useState('all');
+    const [topsProduct, setTopsProduct] = useState([]);
+    const [bottomsProduct, setBottomsProduct] = useState([]);
+    const [miscProduct, setMiscProduct] = useState([]);
+    const [allProduct, setAllProduct] = useState([]);
 
     useEffect(() => {
         axios.get('/products/all')
             .then(res => {
-                setProduct(res.data);
+                const product = res.data;
+                setAllProduct(product);
+                setTopsProduct(product.filter(item => item.category === 'tops'));
+                setBottomsProduct(product.filter(item => item.category === 'bottoms'));
+                setMiscProduct(product.filter(item => item.category === 'accessories'));
             });
     }, []);
 
@@ -24,24 +32,83 @@ function ShowAll() {
             <section className="featured-collections">
                 <header>
                     <h3 className="sub-heading">ALL PRODUCTS</h3>
+                    <div className="tab-button-container">
+                        <button className={activeSection === 'all' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("all")}>ALL</button>
+                        <button className={activeSection === 'tops' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("tops")}>TOPS</button>
+                        <button className={activeSection === 'bottoms' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("bottoms")}>BOTTOMS</button>
+                        <button className={activeSection === 'accessories' ? "tab-item is-active" : "tab-item"} onClick={() => setActiveSection("accessories")}>MISC</button>
+                    </div>
                 </header>
                 <section className='products'>
-                    <>
-                        {product.map((product, index) => (
-                            <article key={index}>
-                                <Image
-                                    to={`/Products/${product.base_sku}`}
-                                    imgDivClass='img-div-home'
-                                    imgClass='product-img-home'
-                                    product={product}
-                                />
-                                <section className='name-and-price'>
-                                    <h3>{product.name}</h3>
-                                    <h4>${(product.price_cents / 100).toFixed(2)}</h4>
-                                </section>
-                            </article>
-                        ))}
-                    </>
+                {activeSection === 'tops' ?
+                        <>
+                            {topsProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to={`/Product/${product.base_sku}`}
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                        : activeSection === 'bottoms' ?
+                        <>
+                            {bottomsProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to={`/Product/${product.base_sku}`}
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                        : activeSection === 'accessories' ?
+                        <>
+                            {miscProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to={`/Product/${product.base_sku}`}
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                        :
+                        <>
+                            {allProduct.map((product, index) => (
+                                <article key={index}>
+                                    <Image
+                                        to={`/Product/${product.base_sku}`}
+                                        imgDivClass='img-div-home'
+                                        imgClass='product-img-home'
+                                        product={product}
+                                    />
+                                    <section className='name-and-price'>
+                                        <h3>{product.name}</h3>
+                                        <h4>${(product.price_cents / 100).toFixed(2)}</h4>
+                                    </section>
+                                </article>
+                            ))}
+                        </>
+                    }
                 </section>
             </section>
         </div>
