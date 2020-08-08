896d68623ad0bcc00888806b7731807fc5d451fa
Gage Eide <gageeide@gmail.com>
2020-08-07 23:28:51 -0500

    Added example data to orders POST route

--- API/src/orders/orders-router.js
+++ API/src/orders/orders-router.js
@@ -6,6 +6,7 @@ const ordersRouter = express.Router();
 const bodyParser = express.json();
 
 // Change product quantity from string to integer
+// !!! USE THIS ON EVERY RESPONSE FROM ORDERS TABLE
 const quantityStrToInt = (ordersArr) => {
 	return ordersArr.map(order => {
 		order.product_skus_and_quantity = order.product_skus_and_quantity.map(product => {
@@ -26,8 +27,17 @@ ordersRouter
 	})
 	.post(bodyParser, async (req, res, next) => {
 		const orderData = req.body;
+		/** Data should contain all of these properties:
+		 * {
+				"email" : "user@test.com",
+				"address" : "123 Address Lane",
+				"state" : "OK",
+				"product_skus_and_quantity" : "{{ATC-OK-Beanie-S-GREEN,1},{ATC-OK-Beanie-M-DKBL,3},{ATC-OK-DadHat-XS,5}}",
+				"amount_cents" : 7900,
+				"uuid" : "a6b14dc5-8102-4d14-8d43-73bf16asd8eec"
+			} */
 		const dbResponse = await ordersService.insertOrder(req.app.get('db'), orderData);
-		res.send(dbResponse);
+		res.send(quantityStrToInt(dbResponse));
 	})
 
 module.exports = ordersRouter;
