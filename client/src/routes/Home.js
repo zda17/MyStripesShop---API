64e363b6435cd0c4e8f34ccbf87594aaa9dfc607
Gage Eide <gageeide@gmail.com>
2020-08-07 19:11:42 -0500

    Squashed errors in Home.js

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -2,11 +2,7 @@ import React from 'react';
 import '../stylesheets/Home.scss';
 import '../stylesheets/Image.scss';
 
-// Axios
-import axios from '../utils/axios';
-
 //components
-import Header from '../components/Header';
 import { Banner } from '../components/Banner';
 import ShopSection from '../components/ShopSection';
 
