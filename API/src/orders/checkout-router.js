25dc81494f97cf8130dd10b8e11aefdac3ba5712
Matthew Wagaman <averaqedev@gmail.com>
2020-08-15 19:07:44 -0700

    add order to orders table after payment

--- API/src/orders/checkout-router.js
+++ API/src/orders/checkout-router.js
@@ -1,37 +1,30 @@
 const express = require('express');
 const checkoutRouter = express.Router();
 const bodyParser = express.json();
-const stripe = require('stripe')('sk_test_51HELKHG3yT4fkVPvD1aAjrBGKkb0uvwSWyUYUB7cxSdsty7J6bqPpDi2lccBIYYCp6liw6BvjxuktAbe7F8qT5Zn00caVHKMQo')
+const stripe = require('stripe')('sk_test_51HELKHG3yT4fkVPvD1aAjrBGKkb0uvwSWyUYUB7cxSdsty7J6bqPpDi2lccBIYYCp6liw6BvjxuktAbe7F8qT5Zn00caVHKMQo');
 
 checkoutRouter
-    .route('/')
-    .post(bodyParser, async (req, res, next) => {
-        console.log(req.body);
-        const { id, amount, uuid } = req.body;
+  .route('/')
+  .post(bodyParser, async (req, res, next) => {
+    const { id, amount, uuid } = req.body;
 
-        try {
-            const payment = await stripe.paymentIntents.create({
-                amount,
-                currency: 'USD',
-                description: uuid,
-                payment_method: id,
-                confirm: true
-            })
+    try {
+      const payment = await stripe.paymentIntents.create({
+        amount,
+        currency: 'USD',
+        description: uuid,
+        payment_method: id,
+        confirm: true
+      });
 
-            console.log(payment);
+      return res.status(200).json({
+        confirm: payment.id //give user id of stripe payment for reference;
+      });
 
-            return res.status(200).json({
-                confirm: 'abc123'
-                //conf code/give user a reference to payment
-            });
-
-        } catch (error) {
-            console.log(error);
-            return res.status(400).json({
-                message: error.message
-            })
-        }
-    })
+    } catch (error) {
+      next(error);
+    }
+  });
 
 
 module.exports = checkoutRouter;
--- client/src/components/Checkout/UserInfoForm.js
+++ client/src/components/Checkout/UserInfoForm.js
@@ -129,7 +129,7 @@ const UserInfoForm = () => {
                 </section>
             }
             {showCheckoutForm &&
-                <Payment />
+                <Payment userInfo={userInfo} />
             }
         </>
     )
