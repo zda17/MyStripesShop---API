7c0b3e75801914a30b54dfeea3f3a33821a052ec
Gage Eide <gageeide@gmail.com>
2020-08-18 16:40:19 -0500

    Adjusted postgres and postgrator versions

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,5 @@ module.exports = {
   'connectionString': (process.env.NODE_ENV === 'test') ?
     process.env.TEST_DATABASE_URL :
     process.env.DATABASE_URL,
-  'ssl': !!process.env.SSL
+  'ssl': { rejectUnauthorized: false }
 };
