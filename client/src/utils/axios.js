ad0ceb7db27ec1e6d663a1ab759829a5cafddbc4
Gage Eide <gageeide@gmail.com>
2020-12-18 15:43:36 -0600

    Changed axios baseUrl to match new URL for api

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'https://mystripes-api.herokuapp.com/api'
+	baseURL: 'https://my-stripes.herokuapp.com/api'
 	
 	// for testing in development:
 	// baseURL: 'http://localhost:8000/api'
